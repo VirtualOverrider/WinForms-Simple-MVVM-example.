ce83de9eb974a563bd9f94a83a827526794a3882
VirtualOverrider <37668880+VirtualOverrider@users.noreply.github.com>
2018-03-22 18:14:38 +0000

    Updated comments

--- WinFormsBindingExample/Views/ExampleView.cs
+++ WinFormsBindingExample/Views/ExampleView.cs
@@ -14,10 +14,14 @@ namespace WinFormsBindingExample.Views
     public partial class ExampleView : UserControl
     {
         #region Private fields
-        private ExampleViewModel _model;                                        // Backing store for the Model this View will bind with.
+        private ExampleViewModel _model;                // Local reference to the Model this View will use for Binding operations.
         #endregion
 
         #region Ctor
+        /// <summary>
+        /// Construct our ExampleView and create its Model ourselves.
+        /// <para>The caller did not pass a ExampleViewModel argument</para>
+        /// </summary>
         public ExampleView()
         {
             _model = new ExampleViewModel();
@@ -26,6 +30,10 @@ namespace WinFormsBindingExample.Views
             PerformBinding();
         }
 
+        /// <summary>
+        /// Construct our ExampleView and use Dependancy Injection to use a passed Model.
+        /// </summary>
+        /// <param name="model">ExampleViewModel to use for Binding</param>
         public ExampleView(ExampleViewModel model)
         {
             _model = model;
@@ -33,13 +41,16 @@ namespace WinFormsBindingExample.Views
 
             PerformBinding();
         }
+        #endregion
 
+        /// <summary>
+        /// Sets up binding for the tbName and lblName controls used in this ExampleView.
+        /// <para>You might want to also implement an interface that ensures all of your View types implement PerformBinding() to keep everything predictable.</para>
+        /// </summary>
         public void PerformBinding()
         {
             tbName.DataBindings.Add("Text", _model, nameof(_model.Name), false, DataSourceUpdateMode.OnPropertyChanged);
             lblName.DataBindings.Add("Text", _model, nameof(_model.Name), false, DataSourceUpdateMode.OnPropertyChanged);
         }
-        #endregion
-
     }
 }

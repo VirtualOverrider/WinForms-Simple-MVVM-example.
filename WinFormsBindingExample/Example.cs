86e1d708dbdbea6afb78570eaeb9d2607b791741
VirtualOverrider <37668880+VirtualOverrider@users.noreply.github.com>
2018-03-22 18:07:19 +0000

    Improved comments

--- WinFormsBindingExample/Example.cs
+++ WinFormsBindingExample/Example.cs
@@ -15,21 +15,23 @@ namespace WinFormsBindingExample
     public partial class Example : Form
     {
         #region Private Fields
-        private ExampleView _view;
-        private ExampleViewModel _model;
+        private ExampleView _view;          // Field used to store our View
+        private ExampleViewModel _model;    // Field used to store our ViewModel
         #endregion
 
         #region Ctor
         public Example()
         {
+            // Create a new ViewModel
             _model = new ExampleViewModel();
+            
+            // Create a new View and inject the ViewModel into it.
             _view = new ExampleView(_model);
 
-
+            // Add the View UserControl to our Form.
             this.Controls.Add(_view);
-            InitializeComponent();
-
             
+            InitializeComponent();
         }
         #endregion
     }
